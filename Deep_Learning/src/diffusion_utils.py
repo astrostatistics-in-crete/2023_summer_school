3fd34175f36f6c852e5486796dbc79a55aae3cf6
gitpeblo <paolobonfini@virgilio.it>
2023-04-05 02:30:57 +0300

    Edited plenty of comments and variable names, and added KL

---
+++ Deep_Learning/src/diffusion_utils.py
@@ -0,0 +1,44 @@
+# Loss Function for Diffusion Model
+# Original Source: https://github.com/acids-ircam/diffusion_models
+
+import torch
+import numpy as np
+
+def make_beta_schedule(schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2):
+    if schedule == 'linear':
+        betas = torch.linspace(start, end, n_timesteps)
+    elif schedule == "quad":
+        betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
+    elif schedule == "sigmoid":
+        betas = torch.linspace(-6, 6, n_timesteps)
+        betas = torch.sigmoid(betas) * (end - start) + start
+    return betas
+
+def extract(input, t, x):
+    shape = x.shape
+    out = torch.gather(input, 0, t.to(input.device))
+    reshape = [t.shape[0]] + [1] * (len(shape) - 1)
+    return out.reshape(*reshape)
+
+def p_sample(model, x, t,alphas,betas,one_minus_alphas_bar_sqrt):
+    t = torch.tensor([t])
+    # Factor to the model output
+    eps_factor = ((1 - extract(alphas, t, x)) / extract(one_minus_alphas_bar_sqrt, t, x))
+    # Model output
+    eps_theta = model(x, t)
+    # Final values
+    mean = (1 / extract(alphas, t, x).sqrt()) * (x - (eps_factor * eps_theta))
+    # Generate z
+    z = torch.randn_like(x)
+    # Fixed sigma
+    sigma_t = extract(betas, t, x).sqrt()
+    sample = mean + sigma_t * z
+    return (sample)
+
+def p_sample_loop(model, shape,n_steps,alphas,betas,one_minus_alphas_bar_sqrt):
+    cur_x = torch.randn(shape)
+    x_seq = [cur_x]
+    for i in reversed(range(n_steps)):
+        cur_x = p_sample(model, cur_x, i,alphas,betas,one_minus_alphas_bar_sqrt)
+        x_seq.append(cur_x)
+    return x_seq
